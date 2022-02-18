34d7ed1ba19f302505e572c9909270b24bc2bd3d
Khramova Anastasiia <anastasiakhrmv@gmail.com>
2022-02-18 15:18:53 +0200

    add create modal

---
+++ packages/web/src/pages/Main/helpers/showModals.ts
@@ -0,0 +1,7 @@
+export function showModals(node) {
+    node.classList.add('showModal');
+}
+
+export function closeModals(node) {
+    node.classList.remove('showModal');
+}
--- packages/web/src/pages/Main/index.ts
+++ packages/web/src/pages/Main/index.ts
@@ -2,9 +2,15 @@ import './styles/styles.scss';
 import { showCloseDropDowns } from '../helper/showCloseDropDowns';
 import { addListener, getNode } from '../helper/utils';
 import { showDatabases, showSorting } from './helpers/showDropDown';
+import { showModals, closeModals } from './helpers/showModals';
+import { validate } from './helpers/validation/validationCreate';
 
 document.addEventListener('DOMContentLoaded', function () {
     showCloseDropDowns();
     addListener('panel', 'click', showDatabases.bind(null, getNode('db'), getNode('mongoDB'), getNode('sectionDB')));
     addListener('panel', 'click', showSorting.bind(null, getNode('sorting'), getNode('fields'), getNode('sortSection')));
+    addListener('create', 'click', showModals.bind(null, getNode('modalCreate')));
+    addListener('closeCreate', 'click', closeModals.bind(null, getNode('modalCreate')));
+    //
+    addListener('modalCreate', 'change', validate);
 })
