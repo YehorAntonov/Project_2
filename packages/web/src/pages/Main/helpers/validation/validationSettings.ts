1152790e78b121a2c75602a564518a9a06296e14
kttolik <kttolikdreamer@gmail.com>
2022-02-19 16:59:49 +0200

    FuckUpOf Validathion

--- packages/web/src/pages/Main/helpers/validation/validationSettings.ts
+++ packages/web/src/pages/Main/helpers/validation/validationSettings.ts
@@ -1,47 +1,23 @@
-import { getNode, getNodeClass, setHTMLValue } from './../../../helper/utils';
+import {getNode, getNodeClass, setHTMLValue} from './../../../helper/utils';
 
 export function validateSettings(e) {
   const checkPassword = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/;
-  const checkLogin =  /^[a-zA-Z'][a-zA-Z-' ]+[a-zA-Z']?$/;
+  const checkLogin = /^[a-zA-Z'][a-zA-Z-' ]+[a-zA-Z']?$/;
 
   if (e.target.id === 'old-password') {
     isValidation(checkPassword, getNode('old-password'), getNodeClass('window-setting__old--input-pass'), 'Wrong value! Check Old password!');
-  } else if (e.target.id === 'new-password' ) {
+  } else if (e.target.id === 'new-password') {
     isValidation(checkPassword, getNode('new-password'), getNodeClass('window-setting__new--input-pass'), 'Wrong value! Check Your New password!');
-  }  else if (e.target.id === 'confirm-password' ) {
+  } else if (e.target.id === 'confirm-password') {
     isValidation(checkPassword, getNode('confirm-password'), getNodeClass('window-setting__confirm--input-pass'), 'Wrong value! Check Your confirm password!');
-  }  else if (e.target.id === 'old-login') {
+    ValidateComparesPasswordSetting(getNode('old-password').value, getNode('new-password').value, getNode('confirm-password').value, 'Wrong value! Check your passwords')
+  } else if (e.target.id === 'old-login') {
     isValidation(checkLogin, getNode('old-login'), getNodeClass('window-setting__old--input-log'), 'Wrong value! Check Old Login');
   } else if (e.target.id === 'new-login') {
-    isValidation(checkLogin,getNode('new-login'), getNodeClass('validateNewLogin'), 'Wrong value! Check New Login');
+    isValidation(checkLogin, getNode('new-login'), getNodeClass('validateNewLogin'), 'Wrong value! Check New Login');
+    ValidateComparesLoginSetting(getNode('new-login').value, getNode('old-login').value, 'Wrong value! Check your Logins')
   }
 }
-
-function ValidateComparesPasswordSetting(value1, value2,  value3, errorText) {
-  if (value1 === value2 || value2 !== value3) {
-    setHTMLValue('errorSettings', errorText);
-  } else {
-    setHTMLValue('errorSettings', '');
-  }
-}
-
-
- function ValidateComparesLoginSetting(value1, value2, errorText) {
-if (value1 === value2) {
-  setHTMLValue('errorSettings', errorText);
-} else {
-  setHTMLValue('errorSettings', '');
-}
-}
-
-
-/*
-
-|| e.target.id === 'confirm-password'
-else if (e.target.id === 'new-login'){
-  comparisonValue(getNode('new-login').value, getNode('old-login').value, getNodeClass('window-setting__old--input-log'), 'Wrong value! Check New Login')
-}*/
-
 function isValidation(reg, valueNode, nodeInput, errorText) {
   if (reg.test(valueNode.value)) {
     setHTMLValue('errorSettings', '')
@@ -51,4 +27,25 @@ function isValidation(reg, valueNode, nodeInput, errorText) {
     nodeInput.classList.add('invalid');
   }
 }
+
+
+function ValidateComparesPasswordSetting(value1, value2, value3, errorText) {
+  if (value1 === value2 || value2 !== value3) {
+    setHTMLValue('errorSettings', errorText);
+  } else {
+    setHTMLValue('errorSettings', '');
+  }
+}
+
+
+function ValidateComparesLoginSetting(value1, value2, errorText) {
+  if (value1 === value2) {
+    setHTMLValue('errorSettings', errorText);
+  } else {
+    setHTMLValue('errorSettings', '');
+  }
+}
+
+
+
 
